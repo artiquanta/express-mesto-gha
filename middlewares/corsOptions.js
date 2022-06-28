bf41241cb37d8d44ca402e89ca1bdc8fdafb352c
quantum <partnerprods@yandex.ru>
2022-06-28 22:38:58 +0700

    fix: разрешить CORS Cookies

--- middlewares/corsOptions.js
+++ middlewares/corsOptions.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
 
     if (method === 'OPTIONS') {
       const requestHeaders = req.headers['access-control-request-headers'];
