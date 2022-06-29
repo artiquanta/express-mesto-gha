8b7718f3a583d20f2e13634efd3d83431b789ab8
quantum <partnerprods@yandex.ru>
2022-06-30 00:44:22 +0700

    fix: изменить сортировку карточек

--- controllers/users.js
+++ controllers/users.js
@@ -56,11 +56,13 @@ module.exports.login = (req, res, next) => {
           maxAge: 604800000,
           httpOnly: true,
           sameSite: true,
+          secure: true,
         })
         .cookie('auth', 'active', {
           domain: 'quantum.nomoredomains.xyz',
           maxAge: 604800000,
           sameSite: true,
+          secure: true,
         })
         .send({ message: 'Вы успешно авторизовались' });
     })
@@ -76,10 +78,12 @@ module.exports.logout = (req, res, next) => {
       domain: 'quantum.nomoredomains.xyz',
       httpOnly: true,
       sameSite: true,
+      secure: true,
     })
     .clearCookie('auth', {
       domain: 'quantum.nomoredomains.xyz',
       sameSite: true,
+      secure: true,
     })
     .send({ message: 'Вы успешно вышли из системы. До скорой встречи' });
 };
